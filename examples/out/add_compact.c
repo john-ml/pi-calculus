e98fdcc4c6cfcb7ccd0644689d35429a40205409
John Li <johnli0135@gmail.com>
2019-11-28 01:00:44 -0500

    Cleanup

--- examples/out/add_compact.c
+++ examples/out/add_compact.c
@@ -1,23 +1,23 @@
 #include <stdlib.h>
 #include "runtime.c"
-// gt_val read_int(void) {
-//   int n = 0;
-//   int c;
-//   do {
-//     c = getchar();
-//   } while (c == '\n' || c == ' ');
-//   while ('0' <= c && c <= '9') {
-//     n = 10*n + (c - '0');
-//     c = getchar();
-//   }
-//   return (gt_val)(long)n;
-// }
-
 gt_val read_int(void) {
+  int n = 0;
   int c;
-  scanf("%d", &c);
-  return (gt_val)(long)c;
+  do {
+    c = getchar();
+  } while (c == '\n' || c == ' ');
+  while ('0' <= c && c <= '9') {
+    n = 10*n + (c - '0');
+    c = getchar();
+  }
+  return (gt_val)(long)n;
 }
+
+// gt_val read_int(void) {
+//   int c;
+//   scanf("%d", &c);
+//   return (gt_val)(long)c;
+// }
 
 void write_int(gt_val c) { printf("%d\n", (int)(long)c); }
 
--- examples/out/cat.c
+++ examples/out/cat.c
@@ -1,9 +1,19 @@
 #include <stdlib.h>
 #include "runtime.c"
-void var_f0(void) {
+void var_f3(void) {
   for (;;) {
-    r15 = getchar();
+    r15 = gt_read(rbx);
     putchar(r15);
+  }
+  asm ("jmp gt_stop\t\n");
+}
+void var_f0(void) {
+  rbx = gt_chan();
+  gt_t var_t4 = gt_go(var_f3, 1048576);
+  var_t4->rbx = rbx;
+  for (;;) {
+    r12 = getchar();
+    gt_write(rbx, r12);
   }
   asm ("jmp gt_stop\t\n");
 }
